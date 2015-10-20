8d0c2c1527a517f6ca4f741a14086f9f72f39256
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-20 19:45:32 -0400

    Updated ExampleFrame class

--- src/ExampleFrame.java
+++ src/ExampleFrame.java
@@ -1,38 +1,31 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 
 
 public class ExampleFrame extends JFrame {
 	// PRIVATE INSTANCE VARIABLES
-	private JPanel _contentPanel;
-	private EmptyBorder _myEmptyBorder;
-	private BorderLayout _myBorderLayout;
+
 	private JLabel _firstNameLabel;
 
 	// CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++
 	public ExampleFrame() {
+		super("Example Frame");
 		this._initialize();
 	}
 
 	// PRIVATE METHODS
 	private void _initialize() {
-		this._myEmptyBorder = new EmptyBorder(5, 5, 5, 5);
-		this._myBorderLayout = new BorderLayout(0, 0);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		this._contentPanel = new JPanel();
-		this._contentPanel.setBorder(this._myEmptyBorder);
-		this._contentPanel.setLayout(this._myBorderLayout);
-		setContentPane(this._contentPanel);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new FlowLayout());
+
 		
-		this._firstNameLabel = new JLabel("First Name:");
-		this._contentPanel.add(this._firstNameLabel, BorderLayout.NORTH);
+		this._firstNameLabel = new JLabel("First Name: ", SwingConstants.CENTER);
+		getContentPane().add(this._firstNameLabel);
 	}
 
 }
